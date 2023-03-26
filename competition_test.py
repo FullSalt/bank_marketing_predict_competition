a50f82e8048c85ea039e09acedda0b39780815ef
FullSalt <masaya.mitsushio@kikagaku.co.jp>
2023-03-26 16:31:17 +0900

    fix

--- competition_test.py
+++ competition_test.py
@@ -10,9 +10,10 @@ from sklearn.metrics import roc_auc_score
 
 # Load the data
 bank_test = pd.read_csv('bank_test.csv')
-
 # result 列のfailを0、successを1に変換
 bank_test['result'] = bank_test['result'].map({'fail': 0, 'success': 1})
+
+st.title('Bank Marketing Test!')
 
 # streamlitでファイルをアップロード
 uploaded_file = st.file_uploader("Choose a CSV file", type="csv")
@@ -20,8 +21,24 @@ uploaded_file = st.file_uploader("Choose a CSV file", type="csv")
 if uploaded_file is not None:
     input_df = pd.read_csv(uploaded_file, dtype=int, header=None)
 
-    st.write('Accuracy：', accuracy_score(bank_test['result'], input_df[0])*100, '%')
-    st.write('Recall：', recall_score(bank_test['result'], input_df[0])*100, '%')
-    st.write('Precision：', precision_score(bank_test['result'], input_df[0])*100, '%')
-    st.write('F1score：', f1_score(bank_test['result'], input_df[0])*100, '%')
-    st.write('AUC：', roc_auc_score(bank_test['result'], input_df[0])*100, '%')
+    score_acc = round(accuracy_score(bank_test['result'], input_df[0])*100, 4)
+    score_rec = round(recall_score(bank_test['result'], input_df[0])*100, 4)
+    score_pre = round(precision_score(bank_test['result'], input_df[0])*100, 4)
+    score_f1 = round(f1_score(bank_test['result'], input_df[0])*100, 4)
+    score_auc = round(roc_auc_score(bank_test['result'], input_df[0])*100, 4)
+
+    # st.write('Accuracy：', score_acc, '%')
+    # st.write('Recall：', score_rec, '%')
+    # st.write('Precision：', score_pre, '%')
+    # st.write('F1score：', score_f1, '%')
+    # st.write('AUC：', score_auc, '%')
+    # 表示オプションを変更
+
+    df_score = pd.DataFrame({'Accuracy': [score_acc],
+                            'Recall': [score_rec],
+                            'Precision': [score_pre],
+                            'F1score': [score_f1],
+                            'AUC': [score_auc]}).T
+
+    st.write(df_score)
+    # print(df_score)
